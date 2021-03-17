517d89369048b68eb4b50b9641d1595d75b125bc
jesseygalje <jesseygalje@users.noreply.github.com>
2021-03-17 16:26:06 +0100

    tablet klaar

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ var dagMoment = new Date();
 var dagNacht = dagMoment.getHours();
 
 if (dagNacht < 6) {
-    document.getElementById("dagdeelimg").style.background = "url('image/nacht.jpg')"; //nacht
+    document.getElementById("dagdeelimg").src = "image/nacht.jpg"; //nacht
 } else if (dagNacht >= 6 && dagNacht < 12) {
-    document.getElementById("dagdeelimg").style.background = "url('image/ochtend.jpg')"; //ochtend
+    document.getElementById("dagdeelimg").src = "image/ochtend.jpg"; //ochtend
 } else if (dagNacht >= 12 && dagNacht < 18) {
-    document.getElementById("dagdeelimg").style.background = "url('image/middag.jpg')"; //middag
+    document.getElementById("dagdeelimg").src = "image/middag.jpg"; //middag
 } else {
-    document.getElementById("dagdeelimg").style.background = "url('image/avond.jpg')"; //avond
+    document.getElementById("dagdeelimg").src = "image/avond.jpg"; //avond
 }
 
 //Afbeelding seizoenen
@@ -17,17 +17,17 @@ var seizoenMoment = new Date();
 var seizoenen = seizoenMoment.getMonth();
 
 if (seizoenen <= 2) {
-    document.getElementById("seizoenimg").style.background = "url('image/winter.jpg')";
+    document.getElementById("seizoenimg").src = "image/winter.jpg";
 } else if (seizoenen > 2 && seizoenen <= 5) {
-    document.getElementById("seizoenimg").style.background = "url('image/lente.jpg')";
+    document.getElementById("seizoenimg").src = "image/lente.jpg";
 } else if (seizoenen > 5 && seizoenen <= 8) {
-    document.getElementById("seizoenimg").style.background = "url('image/zomer.jpg')";
+    document.getElementById("seizoenimg").src = "image/zomer.jpg)";
 } else {
-    document.getElementById("seizoenimg").style.background = "url('image/herfst.jpg')";
+    document.getElementById("seizoenimg").src = "image/herfst.jpg";
 }
 
-document.getElementById("dagdeelimg").style.backgroundRepeat = "no-repeat";
-document.getElementById("seizoenimg").style.backgroundRepeat = "no-repeat";
+// document.getElementById("dagdeelimg").style.backgroundRepeat = "no-repeat";
+// document.getElementById("seizoenimg").style.backgroundRepeat = "no-repeat";
 
 
 
@@ -58,4 +58,4 @@ var huidigeDag = new Array('Zondag', 'Maandag', 'Dinsdag', 'Woensdag', 'Donderda
 document.getElementById('klokdag').innerHTML = huidigeDag[dagMoment.getDay()];
 
 var huidigeMaand = new Array('januari', 'februari', 'maart', 'april', 'mei', 'juni', 'juli', 'augustus', 'september', 'oktober', 'november', 'december');
-document.getElementById('klokdatum').innerHTML = dagMoment.getDate() + ' - ' + huidigeMaand[dagMoment.getMonth()] + ' - ' + dagMoment.getFullYear();
+document.getElementById('klokdatum').innerHTML = dagMoment.getDate() + ' ' + huidigeMaand[dagMoment.getMonth()] + ' ' + dagMoment.getFullYear();
